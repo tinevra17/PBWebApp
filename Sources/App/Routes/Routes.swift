03c3a1e5845c03c92eeebd7b233d9fd83d97f9b7
tinevra17 <tinevra17@hotmail.com>
2017-12-18 15:58:19 -0700

    first commit

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -2,13 +2,16 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
+        
         get("hello") { req in
+            
             var json = JSON()
             try json.set("hello", "world")
             return json
         }
 
         get("plaintext") { req in
+            
             return "Hello, world!"
         }
 
